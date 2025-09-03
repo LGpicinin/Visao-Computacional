0052cfab815bbe3658b02daeb8e42a49258f7001
lgpicinin <lgpicinin@mppr.mp.com>
2025-09-03 16:57:01 -0300

    commit inicial

---
+++ main.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+import os
+
+WORKDIR = os.path.dirname(os.path.abspath(__file__))
+IMAGES_PATH = os.path.join(WORKDIR, 'images_gz2', 'images') # supondo que vamos usar o dataset do kaggle: https://www.kaggle.com/datasets/jaimetrickz/galaxy-zoo-2-images
+
+
+def main ():
+
+    for file in sorted(os.scandir(IMAGES_PATH), key=lambda x: x.name): 
+
+        image = cv.imread (file.path, cv.IMREAD_COLOR)
+
+        image.imshow("galaxy image", image)
+    
+
+if __name__ == '__main__':
+    main ()
